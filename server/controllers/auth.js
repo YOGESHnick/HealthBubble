a56c4e010e099a599129fee47a6ce3586054e286
Yogesh Shamlin Shinanth <yogeshshamlin@gmail.com>
2023-10-07 19:17:43 +0530

    authorization

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -43,12 +43,18 @@ export  const register = async(req,res)=>{
 export const login = async (req,res)=>{
     try {
        const { email,password }=req.body; 
-       const user = await User.findOne({ email : email});
+       const user = await User.findOne({ email : email}); 
+
        if(!user) return res.status(400).json({msg:" User does ot exist !"});
 
        const isMatch = await bcrypt.compare(password,user.password); 
        if(!isMatch) return res.status(400).json({msg:" Invalid credentials !"});
-    } catch (error) {
+
+       const token = jwt.sign({id:user._id},process.env.JWT_SECRET);
+       delete user.password;
+       res.status(200).json({token,user});
+    } 
+    catch (error) {
         console.log(error);
     }
 }
