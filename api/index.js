3ba696eb82e00820017cf41472da3ff8e94c6edc
Yogesh Shamlin Shinanth <yogeshshamlin@gmail.com>
2023-11-13 19:51:58 +0530

    finally connecting to Atlas!

--- api/index.js
+++ api/index.js
@@ -6,6 +6,16 @@ const dotenv = require("dotenv");
 const helmet = require("helmet");
 const morgan = require("morgan");
 
+dotenv.config();
+
+mongoose.connect(
+    process.env.MONGO_URL,
+    // { useNewUrlParser:true,useUnifiedTopology:true}
+    ).then(console.log("Connected to MongoDB Atlas!")
+);
+
+// M I D D L E W A R E 
+
 app.listen(8800, ()=>{
     console.log("Server started at port 8800");
 })
