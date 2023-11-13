1a0be6afaa451e989887add4d7a06df75c578a56
Yogesh Shamlin Shinanth <yogeshshamlin@gmail.com>
2023-11-13 22:51:03 +0530

    testing

--- api/routes/auth.js
+++ api/routes/auth.js
@@ -1,7 +1,27 @@
 const router = require("express").Router();
+const User = require("../models/User");
 
-router.get("/", (req,res)=>{
-    res.send("Vanakkam to auth Route!");
-})
+// R E G I S T E R 
+router.post("/register", async(req,res)=>{
+    // const user = await new User({
+    //     username:"nicky",
+    //     email:"nicky@gmail.com",
+    //     password : "12345678"
+    // })
+    // await user.save();
+    // res.send("ok");
+    const newUser = new User({
+        username:req.body.username,
+        email:req.body.email,
+        password:req.body.password,
+    });
+
+    try{
+        const user = await newUser.save();
+        res.status(200).json(user);
+    }catch(error){
+        console.log(error);
+    }
+});
 
 module.exports=router;
