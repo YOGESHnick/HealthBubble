b14b4d5fc3404d94c6b80735872cdcc4a5c0ef65
Yogesh Shamlin Shinanth <yogeshshamlin@gmail.com>
2023-11-14 19:31:54 +0530

    adding more datafields

--- api/routes/users.js
+++ api/routes/users.js
@@ -1,4 +1,18 @@
 const router = require("express").Router();
+const User = require("../models/User");
+
+// Update User
+router.put("/:id", async(req,res)=>{
+    if(req.body.userId === req.params.id || req.user.isAdmin ){
+
+    }else{
+        return res.status(403).json("You are not allowed to perform this action!");
+    }
+})
+// Delete User
+// get a User
+// follow a User
+// unfollow a User
 
 router.get("/", (req,res)=>{
     res.send("Vanakkam to userRoute!");
