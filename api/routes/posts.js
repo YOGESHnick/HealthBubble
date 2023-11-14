d9ac3273b35302eeefecf5b5547901afbb45688c
Yogesh Shamlin Shinanth <yogeshshamlin@gmail.com>
2023-11-15 01:34:08 +0530

    done with new post and update post!

--- api/routes/posts.js
+++ api/routes/posts.js
@@ -1,7 +1,32 @@
 const router = require("express").Router();
-
-router.get("/",(req,res)=>{
-    res.send("Posts!")
-})
+const Post = require("../models/Post");
+//create post
+router.post("/", async(req,res)=>{
+    const newPost = new Post(req.body);
+    try {
+        const savedPost = await newPost.save();
+        res.status(200).json(savedPost);
+    } catch (error) {
+        res.status(500).json(error);
+    }
+} )
+//update post
+router.put("/:id", async (req, res) => {
+    try {
+      const post = await Post.findById(req.params.id);
+      if (post.userId === req.body.userId) {
+        await post.updateOne({ $set: req.body });
+        res.status(200).json("the post has been updated");
+      } else {
+        res.status(403).json("you can update only your post");
+      }
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  });
+//delete post
+//like post
+//get post
+//get timeline posts
 
 module.exports=router;
