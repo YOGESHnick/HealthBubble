07bfc0533f13deffe5e35a1e580ae149ba34f0fe
Yogesh Shamlin Shinanth <yogeshshamlin@gmail.com>
2023-11-14 22:33:46 +0530

    completed  api for unfollow user!

--- api/routes/users.js
+++ api/routes/users.js
@@ -72,6 +72,27 @@ router.put("/:id/follow",async(req,res)=>{
     }
 })
 // unfollow a User
+router.put("/:id/unfollow",async(req,res)=>{
+    if (req.body.userId !== req.params.id ){
+        try {
+            const user = await User.findById(req.params.id);
+            const currentUser = await User.findById(req.body.userId);
+
+            if(user.followers.includes(req.body.userId)){
+                await user.updateOne( { $pull : { followers:req.body.userId } } );
+                await currentUser.updateOne( { $pull : { following:req.params.id } } );
+                res.status(200).json("unfollwed the user!");
+            }else{
+                res.status(403).json("You dont follow this user");
+            }
+
+        } catch (error) {
+            res.status(500).json(error);
+        }
+    }else{
+        res.status(403).json("You can't follow yourself!");
+    }
+})
 
 router.get("/", (req,res)=>{
     res.send("Vanakkam to userRoute!");
