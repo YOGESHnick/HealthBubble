50f13ac2543cc69cb7a8aa1b3882ab0976925026
Yogesh Shamlin Shinanth <yogeshshamlin@gmail.com>
2023-11-15 16:04:31 +0530

    bug

---
+++ front-end/src/components/sidebar/Sidebar.js
@@ -0,0 +1,23 @@
+import "./sidebar.css";
+import {
+  RssFeed,
+  Chat,
+  PlayCircleFilledOutlined,
+  Group,
+  Bookmark,
+  HelpOutline,
+  WorkOutline,
+  Event,
+  School,
+} from "@material-ui/icons";
+
+export default function Sidebar() {
+  return (
+    <div className="sidebar ">
+      Sidebar
+      <div className="sidebarWrapper">
+        
+      </div>
+    </div>
+  );
+}
