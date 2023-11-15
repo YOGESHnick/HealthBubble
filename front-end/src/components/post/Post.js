29ed33cbb544c55976ef016addeaf8806c3b1d36
Yogesh Shamlin Shinanth <yogeshshamlin@gmail.com>
2023-11-15 18:42:20 +0530

    post mock done

---
+++ front-end/src/components/post/Post.js
@@ -0,0 +1,36 @@
+import React from "react";
+import { MoreVert } from "@material-ui/icons";
+import "./post.css";
+
+export default function Post() {
+  return (
+    <div className="post">
+      <div className="postWrapper">
+        <div className="postTop">
+            <div className="postTopLeft">
+                <img className="postProfileImg" src="/assets/person/1.jpeg" alt="" />
+                <span className="postUsername" >Zanderess </span>
+                <span className="postDate" > 5 min ago</span>
+            </div>
+            <div className="postTopRight">
+                <MoreVert />
+            </div>
+        </div>
+        <div className="postCenter">
+            <span className="postText">Hey! Its my first post</span>
+            <img className="postImg" src="/assets/post/1.jpeg" alt="" />
+        </div>
+        <div className="postBottom">
+            <div className="postBottomLeft" >
+                <img className="likeIcon" src="/assets/like.png" alt=""></img>
+                <img className="likeIcon" src="/assets/heart.png" alt=""></img>
+                <span className="postLikeCounter"> 186 people liked it</span>
+            </div>
+            <div className="postBottomRight" >
+            <span className="postCommentText"> 186 people liked it</span>
+            </div>
+        </div>
+      </div>
+    </div>
+  );
+}
