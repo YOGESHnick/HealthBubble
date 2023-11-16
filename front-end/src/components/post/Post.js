3609bf63618212268c6b07539dc1150624378020
Yogesh Shamlin Shinanth <yogeshshamlin@gmail.com>
2023-11-16 19:43:29 +0530

    debug

--- front-end/src/components/post/Post.js
+++ front-end/src/components/post/Post.js
@@ -20,7 +20,7 @@ export default function Post({post}) {
       <div className="postWrapper">
         <div className="postTop">
             <div className="postTopLeft">
-                <img className="postProfileImg" src={PF+Users.filter((u) => u.id === post?.userId)[0]?.profilePicture} alt="" />
+                <img className="postProfileImg" src={ Users.filter((u) => u.id === post?.userId)[0]?.profilePicture} alt="" />
                 <span className="postUsername">
                   {Users.filter((u) => u.id === post?.userId)[0]?.username}
                 </span>
