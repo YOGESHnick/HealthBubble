252a9e46e3b83cc2d3880f45c991e2a121f109b8
Yogesh Shamlin Shinanth <yogeshshamlin@gmail.com>
2023-11-23 00:34:20 +0530

    added navigation!

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,29 @@
+import { Outlet, Navigate, Route, Routes, useLocation } from "react-router-dom";
+import { Login, Home,Register, Profile, ResetPassword } from "./pages"
+
+function Layout(){
+  const user=null;
+  const location=useLocation();
+
+  return user?.token ? (
+    <Outlet />
+  ):(
+    <Navigate to="/login" state={{ from:location }} replace />
+  )
+}
 
 function App() {
   return (
     <div className="App">
+      <Routes>
+        <Route element={<Layout />}>
+          <Route path="/" element={<Home />} ></Route>
+          <Route path="/profile/:id" element={<Profile />} ></Route>
+        </Route>
+        <Route path="/login" element={<Login />} ></Route>
+        <Route path="/register" element={<Register />} ></Route>
+        <Route path="/reset-password" element={<ResetPassword />} ></Route>
+      </Routes>
     </div>
   );
 }
