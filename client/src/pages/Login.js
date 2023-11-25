59084619dcf145c5d3c8c4efc0a87e90889a2241
Yogesh Shamlin Shinanth <yogeshshamlin@gmail.com>
2023-11-25 11:12:27 +0530

    login

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -2,8 +2,28 @@ import React from 'react';
 
 const Login = () => {
   return (
-    <div>
-      Login
+    <div className="login">
+      <div className="left">
+      <h1 className="header" >HealthBubble</h1>
+      <div className="tag">
+        <img className="social" src="social.png" alt="" />
+      </div>
+      <h3>Connect with your health-focussed buds,</h3>
+      <h3>create a social Bubble</h3>
+      </div>
+      <div className="right">
+      <div className="tag">
+        <img className="logo" src="logo.png" alt=""/>
+        <h2>HealthBubble</h2>
+      </div>
+        <h4>Log in to your account</h4>
+        {/* <h5>Welcome back</h5> */}
+        Email Address
+        <input/>
+        Password
+        <input/>
+        <button>Login</button>
+      </div>
     </div>
   );
 }
